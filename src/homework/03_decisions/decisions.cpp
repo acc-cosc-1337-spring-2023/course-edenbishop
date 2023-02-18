50be0bb3b90d3c95254fac13c9b046f25b0b881c
edenbishop <122990960+edenbishop@users.noreply.github.com>
2023-02-18 17:05:14 +0000

    InComp - HW#3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,53 @@
 //write include statement for decisions header
-
+#include <iostream>
+#include <string>
+using std::string;
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    string gradeStr;
+
+    if (grade >= 90 && grade <= 100)
+        gradeStr = "You recived an A!";
+    
+    else if (grade >= 80 && grade <= 89)
+        gradeStr = "You recived a B.";
+    
+    else if (grade >= 70 && grade <= 79)
+        gradeStr = "You recived a C.";
+    
+    else if (grade >= 60 && grade <= 69)
+        gradeStr = "You recived a D.";
+
+    else
+        gradeStr = "You recived an F.";
+        
+    return gradeStr;
+
+}
+
+string get_letter_grade_using_switch(int grade)
+{
+    string gradeStr;
+    switch (grade)
+    
+    {
+        case 90 ... 100:
+            gradeStr = "You recived an A!";
+    
+        case 80 ... 89:
+            gradeStr = "You recived a B.";
+
+        case 70 ... 79:
+            gradeStr = "You recived a C.";
+    
+        case 60 ... 69:
+            gradeStr = "You recived a D.";
+
+        case 0 ... 59:
+            gradeStr = "You recived an F.";
+
+    }
+    return gradeStr;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,70 @@
 //write include statements
+#include <iostream>
+#include <string>
+#include "decisions.h"
+#include "decisions.cpp"
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	int grade,
+	    answer;
+
+	// Display menu
+	cout << "    MENU    ";
+	cout << "\nWhich method would you like to use to process you grade? \nEnter a number to respond.\n";
+	cout << "1-Letter grade using if";
+	cout << "2-Letter grade using switch";
+	cout << "3-Exit";
+	cin >> answer;
+
+	switch (answer)
+	{
+	    case 1:
+	        cout << "Enter in you grade: ";
+	        cin >> grade;
+
+	        // Check response for correct number
+	        if (grade < 1 || grade > 100)
+			{
+	            cout << "ERROR - Grade should be a number between 1 and 100.";
+		        cout << "Please enter a new number";
+		        cin >> grade;
+			}
+
+			else
+		        cout << "Using If: " << get_letter_grade_using_if(grade);
+
+
+	    case 2:
+
+	        cout << "Enter in you grade: ";
+	        cin >> grade;
+
+	        // Check response for correct number
+	        if (grade < 1 || grade > 100)
+			{
+	            cout << "ERROR - Grade should be a number between 1 and 100.";
+		        cout << "Please enter a new number";
+		        cin >> grade;
+			}
+
+			else
+	            cout << "Using Switch: " << get_letter_grade_using_switch(grade);
+
+	    
+		case 3:
+	        cout << "Stopping program...";
+
+	
+	    default:
+		    // Check for correct input
+	        if (answer < 1 && answer > 3)
+	            cout << "ERROR - Grade should be a number between 1 and 3.";
+		        cout << "Please enter a new number";
+		        cin >> answer;
+
+	}
 	return 0;
 }
