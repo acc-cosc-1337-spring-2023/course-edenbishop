1e6fd922b366e39a2f5f25acd3a744322d0a0a9d
edenbishop <122990960+edenbishop@users.noreply.github.com>
2023-03-05 01:35:36 +0000

    HW #5 - Commit 1

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,10 @@
+//write include required statements
+#include <iostream>
+using namespace std;
+
 //add function(s) prototype here
+double get_gc_content(const string &dna);
+
+string get_reverse_string(string dna );
+
+string get_dna_complement(string dna );
